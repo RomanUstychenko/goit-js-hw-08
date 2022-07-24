9f05a942ad0c5c23e5a5d88f8bc89f381ea35087
RomanUstychenko <roman.ustychenko@gmail.com>
2022-07-24 14:24:30 +0300

    update simple

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,32 @@
-import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm"
+import "simplelightbox/dist/simple-lightbox.min.css"
+import SimpleLightbox from "simplelightbox"
 
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const creatList = galleryItems
+  .map((galleryItem) =>`<a class="gallery__link" href="${galleryItem.original}">
+    <img
+      class="gallery__image"
+      src="${galleryItem.preview}"
+      data-source="${galleryItem.original}"
+      alt="${galleryItem.description}"
+    />
+  </a>`)
+  .join("");
+const gallery = document.querySelector(".gallery");
+gallery.classList.add('gallery__item')
+gallery.insertAdjacentHTML("afterbegin", creatList); 
+
+const simplebox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+    captionPosition: "bottom",
+  });
+  simplebox.on('show.simplelightbox', function () {
+      
+  });
+  simplebox.next();
+
 
 console.log(galleryItems);
