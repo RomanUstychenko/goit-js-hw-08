32839560efb653fc8c372666383b38f04d96c12b
RomanUstychenko <roman.ustychenko@gmail.com>
2022-07-25 09:25:54 +0300

    upd 02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,39 +1,23 @@
 import Player from '@vimeo/player';
 
-// const player = new Player('handstick', {
-//     id: 19231868,
-//     width: 640
-// });
-
-
+var throttle = require('lodash.throttle');
 const iframe = document.querySelector("#vimeo-player");
 const player = new Player(iframe);
-console.log(player)
-player.on(('timeupdate', throttle(1000)), function(sec) {
-    console.log(sec.seconds);
-});
+const LOCALSTORAGE_KEY = localStorage.getItem("videoplayer-current-time");
+console.log(LOCALSTORAGE_KEY);
 
-// player.getVideoTitle().then(function(title) {
-//     console.log('title:', title);
-// });
+player.on('timeupdate', throttle(saveSec, 1000));
+ function saveSec(sec) {
+localStorage.setItem("videoplayer-current-time", sec.seconds)
+console.log(sec.seconds);
 
-
-player.getCurrentTime().then(function(seconds) {
-    console.log(seconds)
+};
+player.setCurrentTime(LOCALSTORAGE_KEY).then(function(seconds) {
 }).catch(function(error) {
-    // an error occurred
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
 });
-
-// player.setCurrentTime(30.456).then(function(seconds) {
-//     // seconds = the actual time that the player seeked to
-// }).catch(function(error) {
-//     switch (error.name) {
-//         case 'RangeError':
-//             // the time was less than 0 or greater than the video’s duration
-//             break;
-
-//         default:
-//             // some other error occurred
-//             break;
-//     }
-// });
